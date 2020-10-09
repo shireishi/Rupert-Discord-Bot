7b20ea5cb2791e6ecb2c9361b23b76f8b405d0df
Elara Jade <63125641+elarajade984@users.noreply.github.com>
2020-10-08 21:17:48 -0700

    Fixed presence

--- test.py
+++ test.py
@@ -1,26 +1,24 @@
 from pypresence import Presence
 import time
-import random
 
-client_id = '746504617507946705'  # Put your Client ID here, this is a fake ID
-RPC = Presence(client_id)  # Initialize the Presence class
-RPC.connect()  # Start the handshake loop
+client_id = '763966672893509642'
 
+class test:
+    state = "test"
+    details = "test"
+    large_image = "test"
+    small_image = "test"
+    large_text = "test"
+    small_text = "test"
+    start = 1602216040
 
-quotes = [
-    "If you can dream it, you can achieve it.",
-    "Either write something worth reading or do something worth writing.",
-    "You become what you believe.",
-    "Fall seven times and stand up eight.",
-    "The best revenge is massive success.",
-    "Eighty percent of success is showing up.",
-    "Life is what happens to you while you’re busy making other plans.",
-    "Strive not to be a success, but rather to be of value.",
-    "The best time to plant a tree was 20 years ago. The second best time is now.",
-    "Everything you’ve ever wanted is on the other side of fear."
-]  # The quotes to choose from
+try :
+    RPC = Presence(client_id)
+    RPC.connect()
+    print("RPC connection successful.")
+except ConnectionRefusedError:
+    raise Exception("Failed RPC connection.")
 
-
-while True:  # The presence will stay on as long as the program is running
-    RPC.update(details="Famous Quote:", state=random.choice(quotes)) #Set the presence, picking a random quote
-    time.sleep(60) #Wait a wee bit
+while True:
+    RPC.update(details="Coding or Jamming out.", large_image='me', start=test.start, large_text="Yes it's me. Stop asking.")
+    time.sleep(15)
