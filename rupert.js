15043e353fb3a9a81f8218f9938fad0711b5e90d
Elara Jade <63125641+elarajade984@users.noreply.github.com>
2020-10-08 19:38:21 -0700

    Update Security Issue

--- rupert.js
+++ rupert.js
@@ -3,7 +3,6 @@ const Discord = require("discord.js");
 const RPC = require("discord-rpc");
 const bot = new Discord.Client();
 
-let token = "NzQ2NTA0NjE3NTA3OTQ2NzA1.X0BSmg.TCxkd0QYGcaitTZSOwj6Ey-UreM";
 let client_id = "746504617507946705";
 
 client.lo
